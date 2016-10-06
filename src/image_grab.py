dd830b75061cc44e42645d4cba3fccc39e431477
Jade Tabony <jat257@cornell.edu>
2016-10-05 19:39:10 -0700

    added docstring

--- src/image_grab.py
+++ src/image_grab.py
@@ -5,8 +5,23 @@ import Image
 from pymongo import MongoClient
 
 
-def imageLinkToS3(image_link, image_title, bucketname):
-    """DOC STRING."""
+def imageLinkToS3(image_link, image_title, s3_connection):
+    """Takes a image from the given link and stores it in the identified S3 bucket.
+
+    This function opens a URL that is provided by the user with urllib, formats
+    it as a PNG file and then stores it in the connected S3 bucket.
+
+    Attributes:
+        image_link (string): Link to REI image in REI media files.
+
+        image_title (string): Title for the image as it will be stored in S3 bucket.
+
+        s3_connection (boto connection): Connection to the appropriate S3 bucket using boto library.
+
+    Todo:
+        * There has to be a better way to access all of the links.
+
+    """
 
     fp = urllib.urlopen(image_link)
     # Load the URL data into an image
@@ -20,10 +35,7 @@ def imageLinkToS3(image_link, image_title, bucketname):
     im2.save(out_im2, 'PNG')
     # Now we connect to our s3 bucket and upload from memory
     # credentials stored in environment AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-    conn = boto.connect_s3()
-    # Connect to bucket and create key
-    b = conn.get_bucket(bucketname)
-    k = b.new_key(image_title + '.png')
+    k = s3_connection.new_key(image_title + '.png')
 
     # Note we're setting contents from the in-memory string provided by cStringIO
     k.set_contents_from_string(out_im2.getvalue())
@@ -34,8 +46,12 @@ if __name__ == '__main__':
     db = client.products
     rei = db.rei
 
+    conn = boto.connect_s3()
+    # Connect to bucket and create key
+    b = conn.get_bucket(backpackbuilder)
+
     for entries in rei.find({}):
         title = entries['title']
         links = entries['img_links'])
         for url in links:
-            imageLinkToS3(link, title, 'backpackbuilder')
+            imageLinkToS3(link, title, b)
